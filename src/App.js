f3115c6c288f80cbe39e279c1d95ca486489e60d
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-15 16:10:46 -0700

    added basics to layout

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          Home
+          About
+          Projects
+          Contact
+          <button>Resume</button>
+        </nav>
+      </header>
+      <div className="main-content">
+        <h3>Hello my name is</h3>
+        <h1>Kimberly Encinas</h1>
+        <h3>I build websites</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
