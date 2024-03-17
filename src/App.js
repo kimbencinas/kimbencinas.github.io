4898ec5b0a1a6e408cd9712b7775f4ff94c30f0d
kimbencinas <kimbencinas@gmail.com>
2024-03-17 16:42:03 -0700

    minor design changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import { Link } from 'react-router-dom';
-import GradientText from './Components/TextDecoration';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav>
-          Home
+        <div className="flex justify-end gap-4 p-4">
+          <Link to="/">Home</Link>
           <Link to ="/about">About</Link>
           <Link to="/projects">Projects</Link>
           <Link to="/contact">Contact</Link>
           <button>Resume</button>
-        </nav>
+        </div>
       </header>
-      <div className="main-content">
-        <h3>Hello, my name is</h3>
-        <h1>Kimberly Encinas</h1>
-        <h3>I build websites</h3>
+      <div className="main-content justify-center items-center p-7">
+        <h3 className="text-2xl">Hello, my name is</h3>
+        <h1 className="block text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-4xl">Kimberly Encinas</h1>
+        <h3 className="text-2xl">I build websites</h3>
       </div>
     </div>
   );
