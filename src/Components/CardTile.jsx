ee1de5faf4c8835d8ed3f1e8edd2b3dd54622501
kimbencinas <kimbencinas@gmail.com>
2024-03-22 16:32:51 -0700

    add project tiles to website

---
+++ src/Components/CardTile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function CardTile({ cardImg, cardTitle, cardDescription }) {
+    return (
+        <div className="bg-gray-400 rounded-lg">
+            <img src={cardImg} />
+            <div>
+                <h2>{cardTitle}</h2>
+                <p>{cardDescription}</p>
+            </div>
+        </div>
+    );
+}
