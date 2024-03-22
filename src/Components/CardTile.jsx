6f3f8350cfd06f3f6d7088bc20961c78e650dc49
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-22 16:50:04 -0700

    fix issue with card tile

--- src/Components/CardTile.jsx
+++ src/Components/CardTile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CardTile({ cardImg, cardTitle, cardDescription }) {
     return (
         <div className="bg-gray-400 rounded-lg">
-            <img src={cardImg} />
+            <img src={cardImg} alt={cardTitle} />
             <div>
                 <h2>{cardTitle}</h2>
                 <p>{cardDescription}</p>
