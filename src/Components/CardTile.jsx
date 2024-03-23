36afb8aad0c580db30ede0c4899695ab5fd15538
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-22 17:32:16 -0700

    add design changes to card tiles

--- src/Components/CardTile.jsx
+++ src/Components/CardTile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CardTile({ cardImg, cardTitle, cardDescription }) {
     return (
-        <div className="bg-gray-400 rounded-lg">
+        <div className="bg-gray-800 rounded-lg h-24 w-48">
             <img src={cardImg} alt={cardTitle} />
             <div>
                 <h2>{cardTitle}</h2>
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import CardTile from '../Components/CardTile';
 
 export default function Projects() {
     return (
         <div>
             <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">Projects</h2>
+            <CardTile
+                cardTitle="hi"
+                cardDescription="my project"
+            />
         </div>
     );
 }
