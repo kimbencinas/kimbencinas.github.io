b4c6888fb8d6eeab2dde7433ddc8928415184371
kimbencinas <kimbencinas@gmail.com>
2024-03-22 22:02:46 -0700

    add styling to card tiles for projects

--- src/Components/CardTile.jsx
+++ src/Components/CardTile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CardTile({ cardImg, cardTitle, cardDescription }) {
     return (
-        <div className="bg-gray-800 rounded-lg h-24 w-48">
+        <div className="bg-gray-800 rounded-lg h-52 w-52 flex flex-col justify-center items-center">
             <img src={cardImg} alt={cardTitle} />
             <div>
                 <h2>{cardTitle}</h2>
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,14 +3,26 @@ import CardTile from '../Components/CardTile';
 
 export default function Projects() {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center mt-5 gap-4">
             <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">Projects</h2>
-            <div className="project-cards">
+            <div className="project-cards flex flex-wrap gap-4">
+                <CardTile
+                    cardTitle="card title #1"
+                    cardDescription="my project #1"
+                />
+                <CardTile
+                    cardTitle="card title #2"
+                    cardDescription="my project #2"
+                />
+                <CardTile
+                    cardTitle="card title #3"
+                    cardDescription="my project #3"
+                />
+                <CardTile
+                    cardTitle="card title #4"
+                    cardDescription="my project #4"
+                />
             </div>
-            <CardTile
-                cardTitle="hi"
-                cardDescription="my project"
-            />
         </div>
     );
 }
