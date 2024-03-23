869c8642bdce63810229ac3b73814f614fe62059
kimbencinas <kimbencinas@gmail.com>
2024-03-22 21:43:58 -0700

    changes to projects component

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import CardTile from '../Components/CardTile';
 
 export default function Projects() {
     return (
         <div>
             <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">Projects</h2>
+            <div className="project-cards">
+            </div>
         </div>
     );
 }
