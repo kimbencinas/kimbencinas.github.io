01e58cb7e46ba80e188de8230119a2fc2cc87182
kimbencinas <kimbencinas@gmail.com>
2024-03-24 20:46:59 -0700

    add info to about me section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Socials from './Components/Socials';
 import Projects from './Pages/Projects';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import placeholderImg from './img/placeholder-image.jpg'
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
         <h1 className="block text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-5xl font-semibold">Kimberly Encinas</h1>
         <h3 className="text-2xl">I build websites</h3>
       </div>
-      <div className="short-bio">
-        <p>I'm a web developer from California who has a passion for building websites and other digital experiences.</p>
+      <div className="short-bio flex ml-6 gap-4">
+        <p className="text-left">I'm a web developer from California who has a passion for building websites and other digital experiences.</p>
+      <img src={placeholderImg} alt="logo" className="justify-end size-64" />
       </div>
       <Socials />
     </div>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,6 +4,10 @@ export default function About() {
     return (
         <div>
             <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">About Me</h2>
+            <div className="p-4">
+                <p>Hello, my name is Kimberly and I have a passion for creating websites. Ever since my CS class in high school, I've developed a love for computers and technology.</p>
+                <p>Besides being a web developer, I love fashion, art, music, and video games.</p>
+            </div>
         </div>
     );
 }
