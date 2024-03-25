8bdba3fdd13e51b361f38fd5229459abd9444095
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-25 15:00:01 -0700

    add design changes to card tiles

--- src/Components/CardTile.jsx
+++ src/Components/CardTile.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function CardTile({ cardImg, cardTitle, cardDescription }) {
     return (
-        <div className="bg-gray-800 bg-opacity-70 rounded-lg h-60 w-60 flex flex-col justify-center items-center">
-            <img src={cardImg} alt={cardTitle} />
-            <div>
+        <div>
+            <div className="bg-gray-800 bg-opacity-70 rounded-lg h-60 w-60 flex flex-col justify-center items-center">
+                <img src={cardImg} alt={cardTitle} />
                 <h2 className="text-lg font-medium text-center">{cardTitle}</h2>
-                <p className="text-md text-center mb-5">{cardDescription}</p>
             </div>
+            <p className="text-md text-center mt-2">{cardDescription}</p>
         </div>
     );
 }
