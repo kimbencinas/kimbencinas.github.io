42cb5f35f43e89b5978ebc6808417944cfdf21f5
kimbencinas <kimbencinas@gmail.com>
2024-03-27 14:02:57 -0700

    made about me page responsive for mobile

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import PlaceholderImg from '../img/placeholder-image.jpg'
+import Socials from '../Components/Socials';
 
 export default function About() {
     return (
         <div>
             <Navbar />
-            <div className="max-w-[800px] mt-[-100px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-                <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">About Me</h2>
-                <div className="p-4">
+            <div className="max-w-[800px] mt-[-100px] w-full h-screen mx-auto text-center flex flex-col justify-center md:text-left gap-5">
+                <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 md:text-4xl sm:text-4xl text-4xl font-semibold">About Me</h2>
+                <img src={PlaceholderImg} className="w-80 md:w-80 sm:w-80 mx-auto" />
+                <div className="md:text-md sm:text-2xl text-2xl p-2">
                     <p>Hello, my name is Kimberly and I have a passion for creating websites. Ever since my CS class in high school, I've developed a love for computers and technology.</p>
                     <p>Besides being a web developer, I love fashion, art, music, and video games.</p>
                 </div>
             </div>
+        <Socials />
         </div>
     );
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <div>
             <Navbar />
-            <div className="max-w-[800px] mt-[-200px] w-full h-screen mx-auto text-center flex flex-col justify-center gap-2">
+            <div className="max-w-[800px] mt-[-200px] w-full h-screen mx-auto text-center flex flex-col justify-center">
                 <h2 className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">Contact</h2>
                 <div className="contact-me">
                     <h5 className="md:text-4xl sm:text-4xl text-4xl font-semibold">Get In Touch</h5>
