75de3e3cd77f1d2509883485c15ffd0feacaae23
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-28 16:33:45 -0700

    made socials component responsive for all breakpoints

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -5,18 +5,18 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 export default function Socials() {
     return (
-        <div className="flex flex-row gap-7 p-4">
+        <div className="flex flex-row gap-7 p-4 justify-center">
             <a href="https://codepen.io/kimmuaeong" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff", }} size="lg" />
+                <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
             </a>
             <a href="https://www.linkedin.com/in/kimberly-encinas-656929252/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedinIn} style={{ color: "#ffffff", }} size="lg" />
+                <FontAwesomeIcon icon={faLinkedinIn} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
             </a>
             <a href="https://www.freecodecamp.org/kimbencinas" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faFreeCodeCamp} style={{ color: "#ffffff", }} size="lg" />
+                <FontAwesomeIcon icon={faFreeCodeCamp} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
             </a>
             <a href="https://github.com/kimbencinas" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff", }} size="lg" />
+                <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
             </a>
         </div>
     );
