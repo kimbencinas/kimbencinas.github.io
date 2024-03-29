77f3b5b67f751a09e0b9f7db543752861c414330
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-29 15:27:05 -0700

    made contact page responsive for tablet & larger screen sizes

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -7,16 +7,16 @@ export default function Socials() {
     return (
         <div className="flex flex-row gap-7 p-4 justify-center">
             <a href="https://codepen.io/kimmuaeong" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
+                <FontAwesomeIcon icon={faCodepen} style={{ color: "#ffffff" }} className="size-5 sm:size-6 md:size-6 lg:size-5" />
             </a>
             <a href="https://www.linkedin.com/in/kimberly-encinas-656929252/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedinIn} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
+                <FontAwesomeIcon icon={faLinkedinIn} style={{ color: "#ffffff" }} className="size-5 sm:size-6 md:size-6 lg:size-5" />
             </a>
             <a href="https://www.freecodecamp.org/kimbencinas" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faFreeCodeCamp} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
+                <FontAwesomeIcon icon={faFreeCodeCamp} style={{ color: "#ffffff" }} className="size-5 sm:size-6 md:size-6 lg:size-5" />
             </a>
             <a href="https://github.com/kimbencinas" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff" }} className="size-5 sm:size-8 md:size-7 lg:size-5" />
+                <FontAwesomeIcon icon={faGithub} style={{ color: "#ffffff" }} className="size-5 sm:size-6 md:size-6 lg:size-5" />
             </a>
         </div>
     );
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,11 +6,11 @@ export default function Contact() {
     return (
         <div>
             <Navbar />
-            <div className="max-w-[800px] sm:mt-[-100px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-                <h2 className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-3xl font-medium">Contact</h2>
+            <div className="max-w-[800px] sm:mt-[-150px] md:mt-[-200px] w-full h-screen mx-auto text-center sm:text-left md:p-6 flex flex-col justify-center">
+                <h2 className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-4xl font-semibold md:mb-5 sm:mb-5">Contact</h2>
                 <div className="contact-me">
-                    <h5 className="md:text-4xl sm:text-4xl text-4xl font-semibold">Get In Touch</h5>
-                    <p className="md:text-md sm:text-2xl text-xl p-3">Feel free to contact me. I'd love to get in touch with you regarding any opportunities or questions you may have.</p>
+                    <h5 className="md:text-4xl sm:text-4xl text-4xl font-semibold md:mb-9 sm:mb-5">Get In Touch</h5>
+                    <p className="md:text-md sm:text-2xl text-xl md:mb-9 sm:mb-5">Feel free to contact me. I'd love to get in touch with you regarding any opportunities or questions you may have.</p>
                     <div>
                         <p className="md:text-md sm:text-2xl text-xl">kimbencinas@gmail.com</p>
                     </div>
