23affa3171fecbe8bc4f336a2122a8dd10594a2b
Kimberly Encinas <kimbencinas@gmail.com>
2024-04-09 20:55:59 -0700

    resume button leads user to resume pdf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-free/css/all.css';
+import ResumePDF from '../Components/ResumePDF.jsx';
+import KimberlyEncinasResume from '../Files/KimberlyEncinasResume.pdf'
 
 export default function Navbar() {
     const [nav, setNav] = useState(false);
 
     const handleNav = () => {
         setNav(!nav)
+    }
+
+    const handleClick = () => {
+        window.open(KimberlyEncinasResume, '_blank');
     }
 
     return (
@@ -18,7 +24,7 @@ export default function Navbar() {
                 <Link to="/about" className="hover:underline underline-offset-4 decoration-white hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-customPurple to-customPink">About</Link>
                 <Link to="/projects" className="hover:underline underline-offset-4 decoration-white hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-customPurple to-customPink">Projects</Link>
                 <Link to="/contact" className="hover:underline underline-offset-4 decoration-white hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-customPurple to-customPink">Contact</Link>
-                <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                <button onClick={handleClick} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
                     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-lg group-hover:bg-opacity-0">
                         Resume
                     </span>
@@ -33,8 +39,8 @@ export default function Navbar() {
                     <Link to="/about" className="hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-customPurple to-customPink p-3 text-xl">About</Link>
                     <Link to="/projects" className="hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-customPurple to-customPink p-3 text-xl">Projects</Link>
                     <Link to="/contact" className="hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-customPurple to-customPink p-3 text-xl">Contact</Link>
-                    <button class="relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-base font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 p-3">
-                        <span class="relative px-4 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-lg group-hover:bg-opacity-0">
+                    <button onClick={handleClick} class="relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-base font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 p-3">
+                        <span className="relative px-4 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-lg group-hover:bg-opacity-0">
                             Resume
                         </span>
                     </button>
